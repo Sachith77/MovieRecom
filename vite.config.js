c0c1507aab5234554d3f6080aa774a7505a0b189
Sachith77 <sachith.24bcs10403@sst.scaler.com>
2025-04-22 19:47:10 +0530

    vc change

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    minify: 'terser',
-    sourcemap: false,
-    rollupOptions: {
-      input: path.resolve(__dirname, './index.html'),
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          animations: ['framer-motion'],
-          threejs: ['three', '@react-three/fiber', '@react-three/drei']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  publicDir: path.resolve(__dirname, '../public'),
-  base: './',
-  server: {
-    port: 3000,
-    open: true
-  }
 })
